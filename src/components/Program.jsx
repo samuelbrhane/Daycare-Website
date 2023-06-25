7404582f4920570ed2d731c1c4a3b4b347d90227
samuelberhane <1samibrhane@gmail.com>
2023-06-25 12:48:53 +0300

    Create Program component.

--- src/components/Program.jsx
+++ src/components/Program.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { ProgramContent } from ".";
 
 const Program = () => {
   return (
-    <section id="Program" className="">
-      <img src="/header-bg.png" alt="cloud" className="w-full" />
+    <section id="Program" className="relative">
+      <img
+        src="/white-bg.png"
+        alt="bg"
+        className="absolute top-[-25px] md:top-[-35px] lg:top-[-40px] xl:top-[-50px] 2xl:top-[-65px] left-0 right-0 w-full"
+      />
+
+      <ProgramContent />
     </section>
   );
 };
